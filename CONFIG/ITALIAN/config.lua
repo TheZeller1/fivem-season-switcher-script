eccd0111e22c8c3ffbbc3751384859ab2ec16e11
TheZeller1 <80457303+TheZeller1@users.noreply.github.com>
2021-08-16 22:26:04 +0200

    Delete CONFIG directory

--- CONFIG/ITALIAN/config.lua
+++
@@ -1,7 +0,0 @@
-Config                                  = {}
-Config.defaultSeason                    = 0             --CHANGE THIS TO CHANGE DEFAULT SEASON: 0 for SUMMER AND 1 for WINTER
---  LANGUAGE TRANSLATION:
-Config.langCommandName                  = 'stagione'    --NAME OF THE COMMAND
-Config.langSummer                       = 'estate'      --NAME OF THE SUMMER
-Config.langWinter                       = 'inverno'     --NAME OF THE WINTER
-Config.langOr                           = 'o'           --TRANSLATE 'or' IN YOUR LANGUAGE
